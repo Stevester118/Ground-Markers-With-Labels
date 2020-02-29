a3e2d3ac3f3c03fa3b3de1d98503fc14ca39b05c
Stephen Cirabisi <Stevester118@gmail.com>
2020-02-29 16:45:47 -0500

    fixed ConfigGroup

--- src/main/java/com/GroundMarkersWithLabels/GroundMarkerConfig.java
+++ src/main/java/com/GroundMarkersWithLabels/GroundMarkerConfig.java
@@ -31,7 +31,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("groundMarker")
+@ConfigGroup("groundMarkerWithLabels")
 public interface GroundMarkerConfig extends Config
 {
 	@Alpha
